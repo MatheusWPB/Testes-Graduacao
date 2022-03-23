373ae0d7e4cd4012e7ce1356c07421f22c4923eb
MatheusWPB <98498520+MatheusWPB@users.noreply.github.com>
2022-03-23 16:53:58 -0300

    a

---
+++ curso python/polimorfismo/Polimorfismo.py
@@ -0,0 +1,10 @@
+'''
+Polimorfismo é o principio que permite que classes derivadas
+    de uma super classe tenha metdos iguais mas com comprtamentos
+    diferentes
+
+Mesma assinatura =  mesma quantidade de parametros 
+
+'''
+
+
