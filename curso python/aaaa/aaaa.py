86d011aab1f3ba71f0d897f8e927e82e6d6ee15b
MatheusWPB <98498520+MatheusWPB@users.noreply.github.com>
2022-03-29 08:35:43 -0300

    a

---
+++ curso python/aaaa/aaaa.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+p = pd.DataFrame()
+
+tabela = pd.read_excel('D:/Documents/matheus/zebra/Equipe_DEV/Testes/Testes-e-anota-es/curso python/aaaa/testar.xlsx')
+print(tabela)
--- curso python/pandas/pandas.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-tabela = pd.read_excel("testar.xlsx")
-display(tabela)
