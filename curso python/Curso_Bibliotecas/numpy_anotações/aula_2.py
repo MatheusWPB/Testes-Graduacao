745878385c021691b3ed86234bdafffb2eb0cf42
MatheusWPB <98498520+MatheusWPB@users.noreply.github.com>
2022-04-02 12:45:21 -0300

    Update aula_2.py

---
+++ curso python/Curso_Bibliotecas/numpy_anotações/aula_2.py
@@ -0,0 +1,8 @@
+#operadores numpy 
+
+import numpy as np
+
+a =  np.sin(np.pi/2) #sempre em radianos, Ex = 90° é igual a pi/2
+b = np.cos (np.pi) #sempre em radianos, Ex = 90° é igual a pi/2
+
+print(a, b)
